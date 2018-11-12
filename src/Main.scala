c673e0de9f4ae42c2669d260936082d69d9588e8
Aaron Nelson <AaronNelson@u.northwestern.edu>
2018-11-12 13:45:38 -0600

    Added base code for packet receiving

--- src/Main.scala
+++ src/Main.scala
@@ -1,9 +1,27 @@
 import scala.collection.mutable.ListBuffer
 
 object Main extends App {
+  //whether to continue to run the background process
+  var process = true
+
+  //name of codes information file
   val codesFile = "codes.txt"
 
+  //spool up
   Codes.init()
+  PacketReceiver.start()
+
   var c = Codes.map.getOrElse("0x000", new Code(null, new ListBuffer[Field]))
   System.out.println(c.name)
+
+  while(true) {
+    Thread.sleep(50)
+    while (PacketReceiver.packetBuffer.nonEmpty) {
+      val k = PacketReceiver.packetBuffer.remove(0)
+      for (by <- k) {
+        print(by + " ")
+      }
+      println()
+    }
+  }
 }
--- src/PacketReceiver.scala
+++ src/PacketReceiver.scala
@@ -1,3 +1,30 @@
-object PacketReceiver {
+import java.net.{DatagramPacket, InetAddress, MulticastSocket}
 
+import scala.collection.mutable.ListBuffer
+
+object PacketReceiver extends Thread {
+  val packetBuffer: ListBuffer[Array[Byte]] = new ListBuffer[Array[Byte]]
+
+  private val mcastPort: Integer = 4876
+  private val mcastAddr: InetAddress = InetAddress.getByName("239.255.60.60")
+  private val mcastSocket: MulticastSocket = new MulticastSocket(mcastPort)
+  try {
+    mcastSocket.joinGroup(mcastAddr)
+  } catch {
+    case e: Exception =>
+      println("Error opening socket. Is everything on/connected?")
+      System.exit(1)
+  }
+
+  /**
+    * Begins dumping packet data to public member packetBuffer
+    */
+  override def run(): Unit = {
+    while (Main.process) {
+      var localBuffer: Array[Byte] = new Array[Byte](80)
+      val packet: DatagramPacket = new DatagramPacket(localBuffer, localBuffer.length)
+      mcastSocket.receive(packet) //dump packet data into localBuffer
+      packetBuffer += localBuffer
+    }
+  }
 }
